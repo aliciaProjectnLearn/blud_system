9886d6f3f594b775a799ff4e4e43457e1bb7feab
Alicia_Bintoro <aliciabintoro15@gmail.com>
2026-04-04 11:00:12 +0700

    fiks tampilan sidebar

--- resources/views/partials/sidebar.blade.php
+++ resources/views/partials/sidebar.blade.php
@@ -238,6 +238,8 @@
             <a class="nav-link" href="{{ route('adminac.teknisi.index') }}">
                 <i class="fas fa-fw fa-user-cog"></i>
                 <span>Manajemen Teknisi</span>
+            </a>
+        </li>
         <li class="nav-item {{ request()->routeIs('adminac.pelanggan.*') ? 'active' : '' }}">
             <a class="nav-link" href="{{ route('adminac.pelanggan.index') }}">
                 <i class="fas fa-fw fa-users"></i>
