68fe7d025538bd3f1d6c0674aa7d4aa7428b02ef
Adang aja <adangnugroho5@gmail.com>
2026-04-20 15:40:17 +0700

    feat: membuat database servis motor dan mobil

---
+++ app/Models/BookingServis.php
@@ -0,0 +1,70 @@
+<?php
+
+namespace App\Models;
+
+use Illuminate\Database\Eloquent\Factories\HasFactory;
+use Illuminate\Database\Eloquent\Model;
+use Illuminate\Support\Str;
+
+class BookingServis extends Model
+{
+    use HasFactory;
+
+    protected $fillable = [
+        'kode_booking',
+        'user_id',
+        'layanan_servis_id',
+        'tipe_kendaraan',
+        'merek_kendaraan',
+        'nomor_plat',
+        'tahun_kendaraan',
+        'keluhan',
+        'tanggal_booking',
+        'jam_booking',
+        'status',
+        'catatan_admin',
+        'teknisi_id',
+    ];
+
+    protected $casts = [
+        'tanggal_booking' => 'date',
+    ];
+
+    protected static function boot()
+    {
+        parent::boot();
+
+        static::creating(function ($model) {
+            if (empty($model->kode_booking)) {
+                $date = now()->format('Ymd');
+                $random = strtoupper(Str::random(4));
+                $model->kode_booking = "BS-{$date}-{$random}";
+            }
+        });
+    }
+
+    public function user()
+    {
+        return $this->belongsTo(User::class, 'user_id');
+    }
+
+    public function layananServis()
+    {
+        return $this->belongsTo(LayananServis::class, 'layanan_servis_id');
+    }
+
+    public function teknisi()
+    {
+        return $this->belongsTo(User::class, 'teknisi_id');
+    }
+
+    public function rincianServis()
+    {
+        return $this->hasMany(RincianServis::class, 'booking_servis_id');
+    }
+
+    public function pembayaranServis()
+    {
+        return $this->hasOne(PembayaranServis::class, 'booking_servis_id');
+    }
+}
